f91483c02f1b47adf759fade728705a63b981c1b
Bruce Parrello <bparrello@figresearch.com>
2021-02-09 21:49:36 -0600

    Moved sample ID tests to shared.

--- src/test/java/org/theseed/threonine/ThrTest.java
+++ src/test/java/org/theseed/threonine/ThrTest.java
@@ -4,15 +4,9 @@
 package org.theseed.threonine;
 
 import static org.hamcrest.MatcherAssert.assertThat;
-import static org.theseed.test.Matchers.*;
-
-import java.io.File;
-import java.io.IOException;
 import static org.hamcrest.Matchers.*;
 
 import org.junit.Test;
-import org.theseed.io.TabbedLineReader;
-import org.theseed.proteins.SampleId;
 
 /**
  * @author Bruce Parrello
@@ -35,37 +29,5 @@ public class ThrTest {
         assertThat(test.getOrigins(), equalTo("A1:B1, A2:B2, A3:B3, A4:B4"));
     }
 
-    @Test
-    public void idTest() {
-        SampleId samp1 = new SampleId("7_D_TasdA_P_asdD_zwf_DasdDdapA_I_6_M1");
-        assertThat(samp1.toStrain(), equalTo("7_D_TasdA_P_asdD_zwf_DasdDdapA"));
-        assertThat(samp1.toString(), equalTo("7_D_TasdA_P_asdD_zwf_DasdDdapA_I_6_M1"));
-        assertThat(samp1.getTimePoint(), equalTo(6.0));
-        SampleId samp2 = new SampleId("7_D_TasdA_P_asdD_zwf_DasdDdapA_I_4p5_M1");
-        assertThat(samp2.toStrain(), equalTo("7_D_TasdA_P_asdD_zwf_DasdDdapA"));
-        assertThat(samp2.toString(), equalTo("7_D_TasdA_P_asdD_zwf_DasdDdapA_I_4p5_M1"));
-        assertThat(samp2.getTimePoint(), equalTo(4.5));
-        assertThat(samp2, lessThan(samp1));
-        assertThat(samp2.getDeletes(), contains("asd", "dapA"));
-        assertThat(samp2.isIPTG(), isTrue());
-        samp2 = new SampleId("7_0_0_A_asdO_000_D000_0_12_M1");
-        assertThat(samp2.getDeletes().size(), equalTo(0));
-        assertThat(samp2.isIPTG(), isFalse());
-    }
-
-    @Test
-    public void testSamplePattern() throws IOException {
-        File testFile = new File("data", "idMap.tbl");
-        try (TabbedLineReader testStream = new TabbedLineReader(testFile)) {
-            for (TabbedLineReader.Line line : testStream) {
-                String strain = line.get(1);
-                boolean iptg = line.getFlag(3);
-                double time = line.getDouble(4);
-                String medium = line.get(5);
-                SampleId sample = SampleId.translate(strain, time, iptg, medium);
-                assertThat(strain, sample.toString(), equalTo(line.get(2)));
-            }
-        }
-    }
 
 }
